54e3082863aee502587b794525bbd9a03d94d90e
yusejeong <ellen6666@naver.com>
2020-07-28 01:17:45 +0900

    ELLEN add signup logic

---
+++ musicianMatching/MuMa/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model=get_user_model()
+        fields=['username','first_name','last_name','email']
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model=get_user_model()
+        fields=['username','email']
--- musicianMatching/musicianMatching/settings.py
+++ musicianMatching/musicianMatching/settings.py
@@ -122,4 +122,6 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL='MuMa.User'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] 
