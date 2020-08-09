7f26bfe8141f025c766b44c628bf94887cfdda20
yusejeong <ellen6666@naver.com>
2020-08-09 17:15:34 +0900

    ELLEN - create abstractuser

--- musicianMatching/accounts/forms.py
+++ musicianMatching/accounts/forms.py
@@ -9,4 +9,4 @@ class CustomUserChangeForm(UserChangeForm):
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model=get_user_model()
-        fields=['username','email']
+        fields=['username','email','address']
